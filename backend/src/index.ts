5bfc03e0045b1baa48ed6bbf6555f577aeb40620
Akashdeep Singh <akashdeep.singh@sifars.com>
2026-08-13 18:16:29 +0530

    fix : logger error that was actually reverted

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,50 +2,47 @@ import { Elysia } from "elysia";
 import { env } from "./config/env";
 import { info, logger, error, debug } from "@rasla/logify";
 import { mainRouter } from "./router";
-import {
-  connectDatabase,
-  closeDatabase,
-} from "./db";
+import { connectDatabase, closeDatabase } from "./db";
 import { runMigrations } from "./db/migrate";
 import { runSeeders } from "./db/seed";
 import { logError } from "./utils/logger";
 
 async function bootstrap() {
-  try {
-    info("Starting application...");
+    try {
+        info("Starting application...");
 
-    await connectDatabase();
-    await runMigrations();
+        await connectDatabase();
+        await runMigrations();
 
-    await runSeeders();
+        await runSeeders();
 
-    const app = new Elysia()
-      .use(logger())
-      .use(mainRouter)
-      .get("/", () => "ok")
-      .listen(env.appConf.port);
+        const app = new Elysia()
+            .use(logger())
+            .onRequest(({ request }) => {
+                info(`REQUEST URL: ${JSON.stringify(request.url)}`);
+            })
+            .use(mainRouter)
+            .get("/", () => "ok")
+            .listen(env.appConf.port);
 
-    info(
-      `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-    );
+        info(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
 
-    const shutdown = async () => {
-      info("Shutting down...");
+        const shutdown = async () => {
+            info("Shutting down...");
 
-      app.stop();
+            app.stop();
 
-      await closeDatabase();
+            await closeDatabase();
 
-      process.exit(0);
-    };
+            process.exit(0);
+        };
 
-    process.on("SIGINT", shutdown);
-    process.on("SIGTERM", shutdown);
+        process.on("SIGINT", shutdown);
+        process.on("SIGTERM", shutdown);
+    } catch (e) {
+        logError(e, "Application startup");
 
-  } catch (e) {
-    logError(e, "Application startup")
-
-    process.exit(1);
-  }
+        process.exit(1);
+    }
 }
 bootstrap();
